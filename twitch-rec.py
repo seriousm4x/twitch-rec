747a81d603d7d203c7081d70eda42a9b10639208
Maxi Quoß <maxi@quoss.org>
2021-06-17 17:53:48 +0200

    rewritten a lot

--- twitch-rec.py
+++ twitch-rec.py
@@ -1,289 +1,176 @@
-#!/usr/bin/env python3
-
 import datetime
-import getopt
-import http.client
 import json
 import logging
 import os
-import requests
-import subprocess
-import sys
-import textwrap
 import time
-import urllib
+import subprocess
+
+import requests
+import streamlink
 
 
-class Colorlog():
-    logging.basicConfig(
-        format='[\033[1;32m%(asctime)s\033[1;0m] [%(levelname)s] %(message)s', level=logging.WARNING)
-    logging.addLevelName(
-        logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
-    logging.addLevelName(
-        logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-    logging.addLevelName(
-        logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+def update_bearer(client_id, client_secret):
+    tokenurl = f"https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials"
+    try:
+        token_response = requests.post(tokenurl)
+        token_response.raise_for_status()
+        token_json_response = token_response.json()
+        bearer = token_json_response["access_token"]
+        return bearer
+    except Exception as e:
+        logger.error(e)
 
 
-class Config():
-    def __init__(self):
-        self.json_streamer = ""
-        self.json_quality = ""
-        self.json_tw_client_id = ""
-        self.json_tw_oauth_token = ""
-        self.json_po_token = ""
-        self.json_po_user = ""
+def check_stream(streamer, client_id, oauth):
+    # Check if user online, 0: online, 1: offline, 2: not found, 3: error
+    url = "https://api.twitch.tv/helix/streams?user_login=" + streamer
+    info = None
+    status = 3
+    try:
+        headers = {
+            "Client-ID": client_id,
+            "Authorization": "Bearer " + oauth
+        }
+        r = requests.get(url, headers=headers, timeout=15)
+        r.raise_for_status()
+        info = r.json()
 
-        self.cli_streamer = ""
-        self.cli_quality = ""
-        self.cli_tw_client_id = ""
-        self.cli_tw_oauth_token = ""
-        self.cli_po_token = ""
-        self.cli_po_user = ""
+        if info.get("status") == 401:
+            status = 401
+        elif info["data"][0]["type"] == "live":
+            status = 0
 
-    def write_json(self, data, jsonfile):
-        with open(jsonfile, "w+") as self.cf:
-            json.dump(data, self.cf, indent=4, separators=(',', ': '))
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        if e.response:
+            if e.response.reason == "Not Found" or e.response.reason == "Unprocessable Entity":
+                status = 2
+    except requests.exceptions.HTTPError:
+        status = 401
+    except IndexError:
+        status = 1
 
-    def read_json(self):
-        with open(config_file, "r") as self.cf:
-            return json.load(self.cf)
-
-    def cli(self):
-        # Create config if it doesn't exist
-        if not os.path.isfile(config_file):
-            self.create_config("", "", "", "", "", "")
-        # Load current config
-        self.data = self.read_json()
-        self.json_streamer = self.data["rec"]["streamer"]
-        self.json_quality = self.data["rec"]["quality"]
-        self.json_tw_client_id = self.data["twitch"]["client_id"]
-        self.json_tw_oauth_token = self.data["twitch"]["oauth"]
-        self.json_po_token = self.data["pushover"]["token"]
-        self.json_po_user = self.data["pushover"]["user"]
-
-        # Read cli args
-        self.opts = getopt.getopt(sys.argv[1:], 'hs:q:c:o:t:u:', [
-                                  'streamer=', 'quality='])[0]
-        for self.opt, self.arg in self.opts:
-            if self.opt in ('-h', '--help'):
-                print(textwrap.dedent("""
-
-                    Usage: python3 twitch_rec.py [OPTION] [ARG]
-
-                    -h, --help        Shows instructions about this script
-                    -s, --streamer    Specifies the twitch streamer to record
-                    -q, --quality     Specifies the quality of the livestream
-                    -c, --client-id   Specifies the twitch client id. Create one here: https://dev.twitch.tv/dashboard/apps
-                    -o, --oauth       Specifies the twitch oauth token. Create one with "streamlink --twitch-oauth-authenticate"
-                    -t, --pu-token    Specifies the pushover app token. Create one at pushover.net
-                    -u, --pu-user     Specifies the pushover user key. Create one at pushover.net
-
-                    Running the script without cli arguments will ask you for all the settings and will create a json config file for you.
-                    If you already have one, it will read the configs from there.
-
-                """))
-                sys.exit(0)
-            elif self.opt in ('-s', '--streamer'):
-                self.cli_streamer = self.arg
-            elif self.opt in ('-q', '--quality'):
-                self.cli_quality = self.arg
-            elif self.opt in ('-c', '--client-id'):
-                self.cli_tw_client_id = self.arg
-            elif self.opt in ('-o', '--oauth'):
-                self.cli_tw_oauth_token = self.arg
-            elif self.opt in ('-t', '--pu-token'):
-                self.cli_po_token = self.arg
-            elif self.opt in ('-u', '--pu-user'):
-                self.cli_po_user = self.arg
-
-        # Set streamer
-        if not self.cli_streamer:
-            if not self.json_streamer:
-                self.streamer = input(
-                    "Which streamer do you want to record?: ")
-            else:
-                self.streamer = self.json_streamer
-        else:
-            self.streamer = self.cli_streamer
-
-        # Set quality
-        if not self.cli_quality:
-            if not self.json_quality:
-                self.quality = input(
-                    "In which quality do you want to record? (If you dont know, just type \"best\"): ")
-            else:
-                self.quality = self.json_quality
-        else:
-            self.quality = self.cli_quality
-
-        # Set twitch client id
-        if not self.cli_tw_client_id:
-            if not self.json_tw_client_id:
-                self.tw_client_id = input("What is your twitch client id?: ")
-            else:
-                self.tw_client_id = self.json_tw_client_id
-        else:
-            self.tw_client_id = self.cli_tw_client_id
-
-        # Set twitch oauth token
-        if not self.cli_tw_oauth_token:
-            if not self.json_tw_oauth_token:
-                self.tw_oauth_token = input(
-                    "What is your twitch oauth token?: ")
-            else:
-                self.tw_oauth_token = self.json_tw_oauth_token
-        else:
-            self.tw_oauth_token = self.cli_tw_oauth_token
-
-        # Set pushover token
-        if not self.cli_po_token:
-            if not self.json_po_token:
-                self.po_token = input("What is your pushover token?: ")
-            else:
-                self.po_token = self.json_po_token
-        else:
-            self.po_token = self.cli_po_token
-
-        # Set pushover user
-        if not self.cli_po_user:
-            if not self.json_po_user:
-                self.po_user = input("What is your pushover user?: ")
-            else:
-                self.po_user = self.json_po_user
-        else:
-            self.po_user = self.cli_po_user
-
-        return self.streamer, self.quality, self.tw_client_id, self.tw_oauth_token, self.po_token, self.po_user
-
-    def create_config(self, streamer, quality, tw_client_id, tw_oauth_token, po_token, po_user):
-        data = {
-            "rec": {
-                "streamer": streamer,
-                "quality": quality
-            },
-            "twitch": {
-                "client_id": tw_client_id,
-                "oauth": tw_oauth_token
-            },
-            "pushover": {
-                "token": po_token,
-                "user": po_user
-            }}
-        self.write_json(data, config_file)
+    return status
 
 
-class TwitchRecorder:
-    def __init__(self):
-        self.refresh = 15.0
-        self.root_path = os.path.dirname(os.path.realpath(__file__))
+def read_settings(_file):
+    with open(_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
 
-        # Read final config
-        self.data = Config().read_json()
-        self.streamer = self.data["rec"]["streamer"]
-        self.quality = self.data["rec"]["quality"]
-        self.tw_client_id = self.data["twitch"]["client_id"]
-        self.tw_oauth_token = self.data["twitch"]["oauth"]
-        self.po_token = self.data["pushover"]["token"]
-        self.po_user = self.data["pushover"]["user"]
 
-    def run(self):
-        # Set recording path
-        self.rec_path = os.path.join(self.root_path, "recs")
-        if(os.path.isdir(self.rec_path) == False):
-            os.makedirs(self.rec_path)
+def write_settings(_file, data):
+    with open(_file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
 
-        # Check interval
-        if(self.refresh < 15):
-            logging.warning(
-                "Interval should not be lower than 15 seconds.")
-            logging.warning("Interval will be set to 15 seconds.")
-            self.refresh = 15
 
-        # Start loop
-        logging.info("Checking for " + self.streamer + " every " + str(self.refresh) +
-                     " seconds. Recording in " + self.quality + " quality.")
-        self.loopcheck()
-
-    def check_user(self):
-        # Check if user online, 0: online, 1: offline, 2: not found, 3: error
-        self.url = "https://api.twitch.tv/helix/streams?user_login=" + self.streamer
-        self.info = None
-        self.status = 3
+def main():
+    while True:
+        # read settings
+        settings_file = "settings.json"
+        settings = read_settings(settings_file)
         try:
-            self.r = requests.get(
-                self.url, headers={"Client-ID": self.tw_client_id,
-                                   "Authorization": "Bearer " + self.tw_oauth_token}, timeout=15)
-            self.r.raise_for_status()
-            self.info = self.r.json()
-            try:
-                if self.info["data"][0]["type"] == "live":
-                    self.status = 0
-            except IndexError:
-                self.status = 1
-        except requests.exceptions.RequestException as e:
-            if e.response:
-                if e.response.reason == "Not Found" or e.response.reason == "Unprocessable Entity":
-                    self.status = 2
-        return self.status
+            settings["rec"]["streamer"]
+            settings["rec"]["quality"]
+            settings["twitch"]["client_id"]
+            settings["twitch"]["client_secret"]
+            logger.debug("Settings passed.")
+        except KeyError:
+            logging.error(
+                "Streamer, quality, client_id and client_secret are mandatory for running the script. Please add them in the settings.json file.")
+        if not settings["twitch"]["oauth"]:
+            bearer = update_bearer(
+                settings["twitch"]["client_id"], settings["twitch"]["client_secret"])
+            if bearer:
+                settings["twitch"]["oauth"] = bearer
+                write_settings(settings_file, settings)
+                logger.debug("Successfully created oauth token.")
 
-    def loopcheck(self):
+
+        # check stream status
+        status = check_stream(
+            settings["rec"]["streamer"], settings["twitch"]["client_id"], settings["twitch"]["oauth"])
+        if status == 401:
+            logger.debug("API request unauthorized. Requesting now token.")
+            bearer = update_bearer(
+                settings["twitch"]["client_id"], settings["twitch"]["client_secret"])
+            if bearer:
+                settings["twitch"]["oauth"] = bearer
+                write_settings(settings_file, settings)
+                logger.debug("Successfully updated oauth token.")
+            continue
+        elif status == 3:
+            logger.error("Unexpected error. Will try again in 5 minutes.")
+            time.sleep(5*60)
+            continue
+        elif status == 2:
+            logger.error("Streamer not found. Invalid streamer or typo.")
+            time.sleep(settings["rec"]["interval"])
+            continue
+        elif status == 1:
+            logger.info(
+                f"{settings['rec']['streamer']} currently offline. Checking again in {settings['rec']['interval']} seconds.")
+            time.sleep(settings["rec"]["interval"])
+            continue
+
+        # stream live
+        logger.info(f"{settings['rec']['streamer']} is live. Recording ...")
+        file_name = settings["rec"]["streamer"] + "_" + \
+            datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".mkv"
+
+        if not os.path.isdir("recordings"):
+            os.mkdir("recordings")
+        video_file = os.path.join("recordings", file_name)
+
+        # open stream and write to file
+        cmd = ["streamlink", f"twitch.tv/{settings['rec']['streamer']}", settings['rec']['quality'], "--twitch-disable-hosting", "--twitch-disable-ads", "--twitch-disable-reruns", "-o", video_file]
+        p = subprocess.Popen(cmd, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
         while True:
-            # Status messages
-            status = self.check_user()
-            if status == 3:
-                logging.error("Unexpected error. Will try again in 5 minutes.")
-                time.sleep(300)
-            elif status == 2:
-                logging.error("streamer not found. Invalid streamer or typo.")
-                time.sleep(self.refresh)
-            elif status == 1:
-                logging.info(self.streamer + " currently offline, checking again in " +
-                             str(self.refresh) + " seconds.")
-                time.sleep(self.refresh)
-            elif status == 0:
-                logging.info(self.streamer + " is live. Recording ...")
-                self.filename = self.streamer + "_" + \
-                    datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss") + ".mkv"
+            output = p.stdout.readline()
+            if output == "" and p.poll() is not None:
+                break
+            if output:
+                logger.info(output.strip())
 
-                # File name
-                self.rec_filename = os.path.join(self.rec_path, self.filename)
 
-                # Start streamlink process
-                subprocess.call(["streamlink", "--twitch-oauth-token",
-                                 self.tw_oauth_token,
-                                 "twitch.tv/" + self.streamer,
-                                 self.quality, "-o", self.rec_filename])
+        # send pushover message
+        if settings["pushover"]["token"] and settings["pushover"]["user"]:
+            data = {
+                "token": settings["pushover"]["token"],
+                "user": settings["pushover"]["user"],
+                "html": "1",
+                "message": f"Recorded: {file_name}"
+            }
+            r = requests.post(
+                "https://api.pushover.net/1/messages.json", data=data)
+            r.raise_for_status()
+            if r.status_code == 200:
+                logger.info("Pushover message sent.")
+            else:
+                logger.info("Pushover message failed.")
 
-                # Send Pushover
-                if self.po_token and self.po_user:
-                    logging.info("Sending pushover message ...")
-                    conn = http.client.HTTPSConnection("api.pushover.net:443")
-                    conn.request("POST", "/1/messages.json",
-                                 urllib.parse.urlencode({"token": self.po_token,
-                                                         "user": self.po_user, "html": "1",
-                                                         "message": "Recorded: " + self.filename, }),
-                                 {"Content-type": "application/x-www-form-urlencoded"})
-                    conn.getresponse()
-
-                # Wait for twitch api to refresh
-                logging.info(
-                    "Waiting 5 minutes for the twitch api to refresh ... (yes, the api is that slow)")
-                time.sleep(300)
-                logging.info("Going back to checking ...")
+        # Wait for twitch api to refresh
+        logger.info("Waiting 5 minutes for the twitch api to refresh...")
+        time.sleep(5*60)
 
 
 if __name__ == "__main__":
-    try:
-        # Define where the config file is located
-        config_file = os.path.join(os.path.dirname(
-            os.path.abspath(__file__)), "config.json")
-        # Update config with cli args and ask user for missing configs
-        streamer, quality, tw_client_id, tw_oauth_token, po_token, po_user = Config().cli()
-        # Write that config to a file
-        Config().create_config(streamer, quality,
-                               tw_client_id, tw_oauth_token, po_token, po_user)
-        # Start the recoding process
-        TwitchRecorder().run()
-    except KeyboardInterrupt:
-        logging.error("Quit by user.")
+    # setup logging
+    if not os.path.isdir("logs"):
+        os.mkdir("logs")
+    log_file = os.path.join("logs", datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log")
+    logger = logging.getLogger('twitch-rec')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(
+        filename=log_file, mode="w", encoding="utf-8")
+    fh.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+
+    main()
